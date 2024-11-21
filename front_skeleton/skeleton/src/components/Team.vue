94d28a8ce75bdd3c641b215020854fb9042d051d
ljh0401 <dlwnsghks0401@gmail.com>
2024-11-21 09:11:43 +0900

    Feat: 좋아하는 팀 틀 완성

--- front_skeleton/skeleton/src/components/Team.vue
+++ front_skeleton/skeleton/src/components/Team.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    src : String
+})
+
+const emit = defineEmits('teamClick')
+
 </script>
 
 <template>
-
+    <img :src="'/src/assets/'+props.src+'.png'" @click="$emit(team, props.src)"/>
 </template>
 
 <style scoped>
--- front_skeleton/skeleton/src/views/FavoriteTeam.vue
+++ front_skeleton/skeleton/src/views/FavoriteTeam.vue
@@ -1,7 +1,28 @@
 <script setup>
+import team from '@/components/Team.vue'
+import { ref } from 'vue';
+
+const teams = ref([
+    {src: "LOTTE"},
+    {src: "LOTTE"},
+])
 </script>
 
 <template>
+
+<fieldset>
+    <div>
+        <button> 전체 </button>
+        <button> 야구 </button>
+        <button> 축구 </button>
+    </div>
+    <div>
+        <team v-for="team in teams" :src="team.src"/>
+    </div>
+</fieldset>
+<fieldset>
+
+</fieldset>
 
 </template>
 
