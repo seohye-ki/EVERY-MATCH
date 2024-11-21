cc03287d824d133c431e131d475830c118988fe6
ljh0401 <dlwnsghks0401@gmail.com>
2024-11-21 15:10:06 +0900

    Fix: match-schedule 받아오는 법(team_id가 아닌 team_name으로) 변경

--- front_skeleton/skeleton/src/components/Match.vue
+++ front_skeleton/skeleton/src/components/Match.vue
@@ -10,7 +10,7 @@ const match = props.match
 
 <template>
     <div>
-        <h2>{{ match.time }}  {{ match.homeTeamId }} vs {{ match.awayTeamId }}</h2>
+        <h2>{{ match.time }}  {{ match.homeTeamName }} vs {{ match.awayTeamName }}</h2>
         <h2>{{ match.location }}</h2>
     </div>
 </template>
